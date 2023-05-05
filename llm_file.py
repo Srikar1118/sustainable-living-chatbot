398f4a825d59a1e08b2b25e55997de51c0d147bb
Chinta Subha Srikar <127307353+Srikar1118@users.noreply.github.com>
2023-05-05 09:48:55 +0530

    Update llm_file.py

--- llm_file.py
+++ llm_file.py
@@ -18,7 +18,7 @@ def add_bg_from_url():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
+             background-image: url("https://cdn.pixabay.com/photo/2023/05/01/10/42/mountains-7962663_960_720.jpg");
              background-attachment: fixed;
              background-size: cover
          }}
